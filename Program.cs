942c744301f6054766f4a73823d7498f53bb1d79
Honey Badger <sadlerav@rambler.ru>
2022-10-26 17:43:07 +0300

    Построение структуры программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class GB_261258_homework_project
+{
+    static Random rnd = new Random();
+    static int array_lenght = rnd.Next(2, 20);
+    static string[] text_array = new string[array_lenght];
+
+    private static void FillArray()
+    {
+
+    }
+
+    private static void ParceArray()
+    {
+
+    }
+
+    private static void ParceString()
+    {
+
+    }
+
+    private static void PrintArray()
+    {
+
+    }
+
+    static void Main()
+    {
+        FillArray();
+        ParceArray();
+        PrintArray();
+    }
+}
